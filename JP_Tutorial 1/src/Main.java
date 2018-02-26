7d099667223398a61f56f95d828b1288cc844b1e
Golgariert <33132289+Golgariert@users.noreply.github.com>
2018-02-26 23:36:52 +0200

    notReallyAHelloWorldProgramButWhatever.class
    This is not really a Hello World xd but you can Enter a "Hello World" so it counts, maybe? xD

---
+++ JP_Tutorial 1/src/Main.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Please enter a line of text please: ");
+        String line = scanner.nextLine();
+        
+        System.out.println("You entered: " + line);
+        
+        
+    }
+    
+}
